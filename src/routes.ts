bc130defc3d1325b3c2e927465526eed80800830
furlanilucas <furlani.lucas7@gmail.com>
2024-10-24 16:35:02 -0300

    Implantação sistema de autenticação backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 const router = Router();
 
-//-- ROTAS USER --
-router.post('/users', new CreateUserController().handle)
+//Login/Users Routes
+
+router.post('/users', new CreateUserController().handle) //users
+router.post('/session', new AuthUserController().handle) //login
+router.get('/userdt',isAuthenticated, new DetailUserController().handle)// User Detail
+
+//Categories Routes
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router }; 
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+import { hash } from 'bcryptjs';
 
 interface UserRequest{
   name: string;
@@ -6,11 +8,41 @@ interface UserRequest{
 }
 
 class CreateUserService{
-  async execute({name, email, password}: UserRequest){
+  async execute({ name, email, password }: UserRequest){
 
-    console.log(name);
+    // verificar se ele enviou um email
+    if(!email){
+      throw new Error("Email incorrect")
+    }
 
-    return { name: name }
+    //Verificar se esse email já está cadastrado na plataforma
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+
+    if(userAlreadyExists){
+      throw new Error("User already exists")
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+      select:{
+        id: true,
+        name: true,       
+        email: true,
+      }
+    })
+
+
+    return user;
   }
 }
 
